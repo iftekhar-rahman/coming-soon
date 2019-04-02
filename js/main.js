1a2d025fe9b4008285e39f70b75e0d48a0043f35
Iftekhar <iftekharmail1@gmail.com>
2019-04-02 10:45:31 -0700

    Another update

--- js/main.js
+++ js/main.js
@@ -2,6 +2,23 @@ jQuery(document).ready(function($){
 
     $('#menu').slicknav();
 
+    // $("#barkio").click({
+    //     $(".slicknav_menu a").removeClass("slicknav_open");
+    // });
+
+    // $("#barkio").click({
+    //     $(".slicknav_menu a").removeClass("slicknav_open");
+    // });
+
+    // $(".slicknav_nav a").on("click", function(){
+    //     $(".slicknav_btn").removeClass("slicknav_collapsed");
+    // });
+    $(".slicknav_nav a").on("click", function(){
+        // $(".slicknav_nav").css("display", "none");
+    });
+
+    // $("#barkio").removeClass("slicknav_open");
+
 
 });
 
